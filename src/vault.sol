c0f2eb922649babbd0d34def0463aab295d14cd0
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-09-10 18:24:54 +0530

    Added _mint() reference

--- src/vault.sol
+++ src/vault.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.13;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/*
+Referenced: https://docs.openzeppelin.com/contracts/5.x/erc20-supply
+ */
 contract ERC20Vault is ERC20 {
     constructor() ERC20("VaultToken", "VT") {
-        _mint(msg.sender, 500);
+        _mint(msg.sender, 500); //tokens minted to account
     }
 }

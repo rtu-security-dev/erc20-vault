e94cf0737054243e02dcb14b8066492027f247a9
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-09-15 22:43:33 +0530

    Add tokenCreation()

--- src/vault.sol
+++ src/vault.sol
@@ -3,14 +3,19 @@ pragma solidity ^0.8.13;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ERC20Vault{
+contract ERC20Vault is ERC20{
     constructor() ERC20("VaultToken", "VT") {  //(Inspired by: https://docs.openzeppelin.com/contracts/5.x/erc20-supply)
     _mint(msg.sender, 500); //tokens minted to caller //(Inspired by: https://docs.openzeppelin.com/contracts/5.x/erc20-supply)
     }
 
-    address Vault;
+    address public Vault;
     mapping(address => uint256) userDeposits;
     string confirmdeposit = "You have successfull deposited to the vault!";
+
+    function tokenCreation(uint256 amount) public returns (string) {
+        _mint(msg.sender, amount);
+        return "Your have created tokens!";
+    }
 
     function deposit(uint256 amount) public returns (string memory) {
         _transfer(msg.sender, Vault, amount); 

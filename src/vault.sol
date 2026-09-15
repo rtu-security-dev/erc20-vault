dedb0a4eab7e0f24d23c7ef3730a74558b556b4e
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-09-15 22:19:24 +0530

    Fixed deposit() to transfer tokens to vault

--- src/vault.sol
+++ src/vault.sol
@@ -5,18 +5,15 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Vault is ERC20 {
     constructor() ERC20("VaultToken", "VT") {  //(Inspired by: https://docs.openzeppelin.com/contracts/5.x/erc20-supply)
+    _mint(msg.sender, 500); //tokens minted to caller //(Inspired by: https://docs.openzeppelin.com/contracts/5.x/erc20-supply)
     }
 
     mapping(address => uint256) userDeposits;
     string confirmdeposit = "You have successfull deposited to the vault!";
 
-    function deposit(uint256 amount) public returns (string memory) {
-        if (userDeposits[msg.sender] > 0) { //user must have a deposit previosuly
-            _mint(msg.sender, amount); //tokens minted to account //(Inspired by: https://docs.openzeppelin.com/contracts/5.x/erc20-supply)
-            userDeposits += amount;
-            return confirmdeposit;
-        } else {
-            return "You have no tokens to deposit!";
-        }
+    function deposit(address receiver, uint256 amount) public returns (string memory) {
+        _transfer(msg.sender, receiver, amount); 
+        userDeposits[msg.sender] += amount;
+        return confirmdeposit;
     }
 }

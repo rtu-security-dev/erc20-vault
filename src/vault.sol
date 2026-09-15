0df074ac24325f985cada972537f68dc97f32513
Rivindi Udugama <rudugama@our.ecu.edu.au>
2026-09-15 22:25:45 +0530

    Add vault as the fixed receiver in deposit()

--- src/vault.sol
+++ src/vault.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ERC20Vault is ERC20 {
+contract ERC20Vault{
     constructor() ERC20("VaultToken", "VT") {  //(Inspired by: https://docs.openzeppelin.com/contracts/5.x/erc20-supply)
     _mint(msg.sender, 500); //tokens minted to caller //(Inspired by: https://docs.openzeppelin.com/contracts/5.x/erc20-supply)
     }
@@ -11,8 +11,8 @@ contract ERC20Vault is ERC20 {
     mapping(address => uint256) userDeposits;
     string confirmdeposit = "You have successfull deposited to the vault!";
 
-    function deposit(address receiver, uint256 amount) public returns (string memory) {
-        _transfer(msg.sender, receiver, amount); 
+    function deposit(uint256 amount) public returns (string memory) {
+        _transfer(msg.sender, ERC20Vault, amount); 
         userDeposits[msg.sender] += amount;
         return confirmdeposit;
     }
